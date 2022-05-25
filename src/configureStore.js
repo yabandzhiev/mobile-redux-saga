ebba6732c6b75c695e9556b3588c6adf7a86a1fd
yabandzhiev <yabandzhiev@abv.bg>
2022-05-25 10:04:00 +0300

    blacklist errorpopup

--- src/configureStore.js
+++ src/configureStore.js
@@ -10,7 +10,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
   storage: storage,
-  blacklist: ["redirect", "getCars"],
+  blacklist: ["redirect", "getCars", "getErrorPopup"],
 };
 
 const persistedReducer = persistReducer(persistConfig, createReducer());
